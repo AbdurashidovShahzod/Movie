9b977c7a0e921185332f332d94e9d3972ce55faf
AbdurashidovShahzod <shahzod9933@gmail.com>
2021-01-04 21:54:22 +0500

    data

--- app/src/main/java/uz/unzosoft/movie/adapters/DemoAdapter1.kt
+++ app/src/main/java/uz/unzosoft/movie/adapters/DemoAdapter1.kt
@@ -4,27 +4,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.demo_rv_list.view.*
 import uz.unzosoft.movie.R
 import uz.unzosoft.movie.models.DemoMovie
 
-class DemoAdapter1 : RecyclerView.Adapter<DemoAdapter1.MyViewHolder>() {
+class DemoAdapter1(var list: List<DemoMovie>) : RecyclerView.Adapter<DemoAdapter1.MyViewHolder>() {
 
-    var list = ArrayList<DemoMovie>()
-
-    fun addList(list: ArrayList<DemoMovie>) {
-        this.list = list
-        notifyDataSetChanged()
-    }
+//    var list = ArrayList<DemoMovie>()
+//
+//    fun addList(list: ArrayList<DemoMovie>) {
+//        this.list = list
+//        notifyDataSetChanged()
+//    }
 
     inner class MyViewHolder(var view: View) : RecyclerView.ViewHolder(view) {
         fun bind(position: Int) {
             val movie = list[position]
-            view.title.text = list[position].title
-            view.view.text = list[position].view
-            view.releaseData.text = list[position].releaseData
-
-
+            view.title.text = movie.title
+            view.view.text = movie.view
+            view.releaseData.text = movie.releaseData
+            Glide.with(view.context)
+                .load(list[position].image)
+                .into(view.findViewById(R.id.imageDemo))
         }
     }
 
@@ -35,7 +37,7 @@ class DemoAdapter1 : RecyclerView.Adapter<DemoAdapter1.MyViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-
+        holder.bind(position)
     }
 
     override fun getItemCount(): Int = list.size
